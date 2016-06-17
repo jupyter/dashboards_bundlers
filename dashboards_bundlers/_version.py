22bf7d93637510e5f78f7d3f891dfe78e9133f11
Peter Parente <pparent@us.ibm.com>
2016-06-17 14:03:43 -0400

    Bump to 0.8.1.dev

--- dashboards_bundlers/_version.py
+++ dashboards_bundlers/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 8, 0)
+version_info = (0, 8, 1, 'dev')
 __version__ = '.'.join(map(str, version_info))
